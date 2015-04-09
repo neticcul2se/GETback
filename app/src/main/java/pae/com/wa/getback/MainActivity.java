c640151cc04fd055e3852922d020650fe501da1e
Pae <Pae@it-lab-network-92.it-lab-network.mut.ac.th>
2015-04-09 14:15:04 +0700

    Initial commit

---
+++ app/src/main/java/pae/com/wa/getback/MainActivity.java
@@ -0,0 +1,92 @@
+package pae.com.wa.getback;
+
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class MainActivity extends Activity {
+
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Permission StrictMode
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        // txtResult
+        final TextView result = (TextView)findViewById(R.id.txtResult);
+        // btnGetData
+        final Button getData = (Button) findViewById(R.id.btnGetData);
+
+        // Perform action on click
+        getData.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+              String url = "http://httpbin.org/get";
+               String resultServer  = getHttpGet(url);
+               result.setText(resultServer);
+
+
+            }
+        });
+
+    }
+
+
+    public String getHttpGet(String url) {
+        StringBuilder str = new StringBuilder();
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet(url);
+
+
+
+        try {
+
+            HttpResponse response = client.execute(httpget);
+            StatusLine statusLine = response.getStatusLine();
+            int statusCode = statusLine.getStatusCode();
+            if (statusCode == 200) { // Status OK
+                HttpEntity entity = response.getEntity();
+                InputStream content = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    str.append(line);
+                }
+            } else {
+                Log.e("Log", "Failed to download result..");
+            }
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str.toString();
+    }
+
+
+}
